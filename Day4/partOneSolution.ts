6c4d865e0f04f1e0d952501de0a511be4670d736
irhel <ielezovikj@gmail.com>
2025-12-04 06:51:38 +0100

    Day4

---
+++ Day4/partOneSolution.ts
@@ -0,0 +1,56 @@
+import * as readline from 'readline';
+import * as fs from 'fs';
+
+const fileStream = fs.createReadStream(`${__dirname}/input.txt`);
+
+const rl = readline.createInterface({
+  input: fileStream,
+  crlfDelay: Infinity
+});
+
+let g: string[][] = [];
+
+function calculate(g: string[][], rows: number, columns: number) : number {
+
+  let res: number = 0;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+
+      if (g[i][j] != '@') continue;
+      // check 8 directions
+      let deltaX: number[] = [0, 0, -1, -1, -1, 1, 1, 1]
+      let deltaY: number[] = [1, -1, 0, -1, 1, 0, -1, 1]
+      let rolls:number = 0;
+
+      for(let ii = 0; ii < 8; ii++) {
+        if ((i + deltaX[ii] >= 0 && i + deltaX[ii] < rows) && (j + deltaY[ii] >= 0 && j + deltaY[ii] < columns)) {
+
+          // valid position
+          if (g[i + deltaX[ii]][j + deltaY[ii]] === '@')
+            rolls++;
+        }
+      }
+      
+      if (rolls < 4) {
+        res++;
+      }
+    }
+  }
+
+  return res;
+}
+
+rl.on('line', (line: string) => {
+  g.push(line.split(''));
+});
+
+rl.on('close', () => {
+
+  let columns: number = g[0].length;
+  let rows: number = g.length;
+
+  let result: number = calculate(g, rows, columns);
+
+  console.log(result);
+});
