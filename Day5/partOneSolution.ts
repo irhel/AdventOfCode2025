65c375b2bb4987a9575186675b3f9cfb614a2084
irhel <ielezovikj@gmail.com>
2025-12-06 08:10:13 +0100

    Day 5

---
+++ Day5/partOneSolution.ts
@@ -0,0 +1,46 @@
+import * as readline from 'readline';
+import * as fs from 'fs';
+
+const fileStream = fs.createReadStream(`${__dirname}/input.txt`);
+
+const rl = readline.createInterface({
+  input: fileStream,
+  crlfDelay: Infinity
+});
+
+type Range = {
+  start: number,
+  end: number;
+}
+
+let ranges: Range[] = [];
+let numberOfFreshIngredients: number = 0;
+let parseRange: boolean = true;
+
+rl.on('line', (line: string) => {
+
+  if (line === '') {
+    parseRange = false;
+    return;
+  }
+    
+
+  if (parseRange) {
+    let [start, end] = line.split('-').map(rangeElement => Number(rangeElement))
+    ranges.push({ start, end });
+  } else {
+    console.log(`considering ${line} as ingredient`)
+    let ingredientID = Number(line);
+    for (const range of ranges ) {
+      if (ingredientID >= range.start && ingredientID <= range.end) {
+        numberOfFreshIngredients++;
+        break;
+      }
+    }
+  }
+
+});
+
+rl.on('close', () => {
+  console.log(numberOfFreshIngredients)
+});
